bbc8c85e009878d4e3f69aad87d53f18ed675578
NguyenQuocTruong14110425 <14110425@student.hcmute.edu.vn>
2019-01-02 10:02:27 +0700

    file commit  3

---
+++ src/demo_project/demo_file_3.java
@@ -0,0 +1,21 @@
+package demo_project;
+
+import java.util.*;
+
+public class demo_file_3 {
+
+	public String hash;
+	public String previousHash;
+	private String data;
+	private long timeStamp;
+
+	public demo_file_3(String data, String previousHash)
+	{
+		this.data = data;
+	}
+
+	public String hello()
+	{
+		return "hello";
+	}
+}
