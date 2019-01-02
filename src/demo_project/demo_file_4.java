47011758da9c3f6480ed6481f2ee77724f2b576e
NguyenQuocTruong14110425 <14110425@student.hcmute.edu.vn>
2019-01-02 10:27:05 +0700

    commit 9

---
+++ src/demo_project/demo_file_4.java
@@ -0,0 +1,17 @@
+package demo_project;
+
+import java.util.*;
+
+public class demo_file_4 {
+
+	public String hash;
+	public String previousHash;
+	private String data;
+	private long timeStamp;
+
+	public demo_file_4(String data, String previousHash)
+	{
+		this.data = data;
+	}
+	
+}
